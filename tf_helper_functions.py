0025fc04d55da1067fa6513f5af6fb22e79be3bb
gregorywmorris <gwm1980@gmail.com>
2024-04-20 18:07:51 -0400

    food vision

--- tf_helper_functions.py
+++ tf_helper_functions.py
@@ -1,3 +1,5 @@
+"""Run list_helpers() for a list of all helper functions."""
+
 import datetime
 import itertools
 import os
@@ -8,6 +10,22 @@ import tensorflow as tf
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from sklearn.metrics import confusion_matrix
 
+
+def list_helpers():
+    """
+    List the functions in helper functions
+    """
+    print(
+      '1. load_and_prep_image(filename img_shape=224 scale=True) \
+      \n2. make_confusion_matrix(y_true y_pred classes=None figsize=(10 10) text_size=15 norm=False savefig=False) \
+      \n3. pred_and_plot(model filename class_names) \
+      \n4. create_tensorboard_callback(dir_name experiment_name) \
+      \n5. plot_loss_curves plot_loss_curves(history) \
+      \n6. compare_histories(original_history new_history initial_epochs=5) \
+      \n7. unzip_data(filename,location="./") \
+      \n8. walk_through_dir(dir_path) \
+      \n9. calculate_results(y_true y_pred)'
+      )
 
 # Create a function to import an image and resize it to be able to be used with our model
 def load_and_prep_image(filename, img_shape=224, scale=True):
@@ -39,82 +57,82 @@ def load_and_prep_image(filename, img_shape=224, scale=True):
 
 # Function needs a different name to sklearn's plot_confusion_matrix
 def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
-  """Makes a labelled confusion matrix comparing predictions and ground truth labels.
+    """Makes a labelled confusion matrix comparing predictions and ground truth labels.
 
-  If classes is passed, confusion matrix will be labelled, if not, integer class values
-  will be used.
+    If classes is passed, confusion matrix will be labelled, if not, integer class values
+    will be used.
 
-  Args:
-    y_true: Array of truth labels (must be same shape as y_pred).
-    y_pred: Array of predicted labels (must be same shape as y_true).
-    classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
-    figsize: Size of output figure (default=(10, 10)).
-    text_size: Size of output figure text (default=15).
-    norm: normalize values or not (default=False).
-    savefig: save confusion matrix to file (default=False).
-  
-  Returns:
-    A labelled confusion matrix plot comparing y_true and y_pred.
+    Args:
+      y_true: Array of truth labels (must be same shape as y_pred).
+      y_pred: Array of predicted labels (must be same shape as y_true).
+      classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+      figsize: Size of output figure (default=(10, 10)).
+      text_size: Size of output figure text (default=15).
+      norm: normalize values or not (default=False).
+      savefig: save confusion matrix to file (default=False).
 
-  Example usage:
-    make_confusion_matrix(y_true=test_labels, # ground truth test labels
-                          y_pred=y_preds, # predicted labels
-                          classes=class_names, # array of class label names
-                          figsize=(15, 15),
-                          text_size=10)
-  """  
-  # Create the confustion matrix
-  cm = confusion_matrix(y_true, y_pred)
-  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
-  n_classes = cm.shape[0] # find the number of classes we're dealing with
+    Returns:
+      A labelled confusion matrix plot comparing y_true and y_pred.
 
-  # Plot the figure and make it pretty
-  fig, ax = plt.subplots(figsize=figsize)
-  cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
-  fig.colorbar(cax)
+    Example usage:
+      make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                            y_pred=y_preds, # predicted labels
+                            classes=class_names, # array of class label names
+                            figsize=(15, 15),
+                            text_size=10)
+    """
+    # Create the confustion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+    n_classes = cm.shape[0] # find the number of classes we're dealing with
 
-  # Are there a list of classes?
-  if classes:
-    labels = classes
-  else:
-    labels = np.arange(cm.shape[0])
-  
-  # Label the axes
-  ax.set(title="Confusion Matrix",
-         xlabel="Predicted label",
-         ylabel="True label",
-         xticks=np.arange(n_classes), # create enough axis slots for each class
-         yticks=np.arange(n_classes), 
-         xticklabels=labels, # axes will labeled with class names (if they exist) or ints
-         yticklabels=labels)
-  
-  # Make x-axis labels appear on bottom
-  ax.xaxis.set_label_position("bottom")
-  ax.xaxis.tick_bottom()
+    # Plot the figure and make it pretty
+    fig, ax = plt.subplots(figsize=figsize)
+    cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
+    fig.colorbar(cax)
 
-  ### Added: Rotate xticks for readability & increase font size (required due to such a large confusion matrix)
-  plt.xticks(rotation=70, fontsize=text_size)
-  plt.yticks(fontsize=text_size)
-
-  # Set the threshold for different colors
-  threshold = (cm.max() + cm.min()) / 2.
-
-  # Plot the text on each cell
-  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-    if norm:
-      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
-              horizontalalignment="center",
-              color="white" if cm[i, j] > threshold else "black",
-              size=text_size)
+    # Are there a list of classes?
+    if classes:
+      labels = classes
     else:
-      plt.text(j, i, f"{cm[i, j]}",
-              horizontalalignment="center",
-              color="white" if cm[i, j] > threshold else "black",
-              size=text_size)
+      labels = np.arange(cm.shape[0])
 
-  # Save the figure to the current working directory
-  if savefig:
-    fig.savefig("confusion_matrix.png")
+    # Label the axes
+    ax.set(title="Confusion Matrix",
+          xlabel="Predicted label",
+          ylabel="True label",
+          xticks=np.arange(n_classes), # create enough axis slots for each class
+          yticks=np.arange(n_classes),
+          xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+          yticklabels=labels)
+
+    # Make x-axis labels appear on bottom
+    ax.xaxis.set_label_position("bottom")
+    ax.xaxis.tick_bottom()
+
+    ### Added: Rotate xticks for readability & increase font size (required due to such a large confusion matrix)
+    plt.xticks(rotation=70, fontsize=text_size)
+    plt.yticks(fontsize=text_size)
+
+    # Set the threshold for different colors
+    threshold = (cm.max() + cm.min()) / 2.
+
+    # Plot the text on each cell
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+      if norm:
+        plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+                horizontalalignment="center",
+                color="white" if cm[i, j] > threshold else "black",
+                size=text_size)
+      else:
+        plt.text(j, i, f"{cm[i, j]}",
+                horizontalalignment="center",
+                color="white" if cm[i, j] > threshold else "black",
+                size=text_size)
+
+    # Save the figure to the current working directory
+    if savefig:
+      fig.savefig("confusion_matrix.png")
 
 # Make a function to predict on images and plot them (works with multi-class)
 def pred_and_plot(model, filename, class_names):
@@ -141,7 +159,7 @@ def pred_and_plot(model, filename, class_names):
 
 def create_tensorboard_callback(dir_name, experiment_name):
     """
-    Creates a TensorBoard callback instand to store log files.
+    Creates a TensorBoard callback instance to store log files.
 
     Stores log files with the filepath:
       "dir_name/experiment_name/current_datetime/"
@@ -190,7 +208,7 @@ def plot_loss_curves(history):
     plt.xlabel('Epochs')
     plt.legend()
 
-def compare_historys(original_history, new_history, initial_epochs=5):
+def compare_histories(original_history, new_history, initial_epochs=5):
     """
     Compares two TensorFlow model History objects.
     
